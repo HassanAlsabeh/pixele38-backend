cfb5c062055ca852ec379867ea2c30356e547857
Hassan Alsabeh <sabehf71@gmail.com>
2021-12-29 18:41:24 +0200

    updated

--- database/seeders/ShipmentSeeder.php
+++ database/seeders/ShipmentSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Shipments;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class ShipmentSeeder extends Seeder
@@ -13,6 +15,18 @@ class ShipmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\User::all()->each(
+            function($user){ for($i=0;$i<=20;$i++){
+                Shipments::create([
+                    'user_id'=>$user->getKey(),
+                    'waybill'=>'omt'.$i,
+                    'customer_address'=>"Beirut".$i,
+                    'customer_name'=>"Name".$i,
+                    'customer_phone'=>"7191667".$i,
+                ]);
+            }}
+        );
     }
+
 }
+
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,25 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Hassan',
+            'email' => 'hassan@gmail.com',
+            'password' => bcrypt('123456789'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Hassan',
+            'email' => 'hassan1@gmail.com',
+            'password' => bcrypt('123456789'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Hassan',
+            'email' => 'hassan2@gmail.com',
+            'password' => bcrypt('123456789'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Hassan',
+            'email' => 'hassan3@gmail.com',
+            'password' => bcrypt('123456789'),
+        ]);
     }
 }
